aa4341a480ba9c811fcb9420933d4766f92d0b30
Tyler McLean <tmclea03@uoguelph.ca>
2018-03-22 17:29:50 -0400

    Initial Commit

---
+++ NHLStatTrack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NHLStatTrack
+//
+//  Created by Tyler McLean on 2018-03-22.
+//  Copyright © 2018 UofG Students. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
